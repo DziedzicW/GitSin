26e0e36943aa853ad5101df4cd3b312dff834d86
DziedzicW <WojtekDz.@Developers-MacBook-Pro.local>
2016-01-25 11:57:53 +0100

    Nic

---
+++ GitSin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GitSin
+//
+//  Created by Wojciech Dziedzic on 25.01.2016.
+//  Copyright © 2016 Wojciech Dziedzic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
